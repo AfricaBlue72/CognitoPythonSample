90f5f3f46fdce66e3e7e572497fe6c01e1b0032c
AfricaBlue72 <akrijten@gmail.com>
2021-04-14 20:46:49 +0200

    Created

---
+++ CognitoSample.py
@@ -0,0 +1,96 @@
+#!/bin/env python3
+import boto3
+import json
+import argparse
+from datetime import datetime
+import time
+from CognitoUser import CognitoUser
+import uuid
+
+parser = argparse.ArgumentParser(description='Test de user login')
+parser.add_argument('-u', '--userPool', metavar='userPool', type=str, required=True, help='The cognito user pool being used')
+parser.add_argument('-c', '--client', metavar='client', type=str, required=True, help='The cognito client app')
+parser.add_argument('-n', '--userName', metavar='userName', type=str, required=True, help='User name')
+parser.add_argument('-p', '--password', metavar='password', type=str, required=True, help='Password')
+parser.add_argument('-idp', '--identityPoolId', metavar='identityPoolId', type=str, required=True, help='The identity pool Id of the account you want to access resources in.')
+
+parser.add_argument('-t', '--topicArn', metavar='topicArn', type=str, required=True, help='The ARN of the topic to place the events on')
+
+args = parser.parse_args()
+
+def main():
+    print("starting")
+
+    user = CognitoUser(
+        username=args.userName, 
+        password=args.password, 
+        user_pool_id=args.userPool, 
+        client_id=args.client, 
+        identity_provider_id=args.identityPoolId)
+        
+    user.sign_in()
+    
+    temp_credentials = user.get_temporary_credentials()
+    
+    client = boto3.client('sns',
+                          aws_access_key_id=temp_credentials['Credentials']['AccessKeyId'],
+                          aws_secret_access_key=temp_credentials['Credentials']['SecretKey'],
+                          aws_session_token=temp_credentials['Credentials']['SessionToken'])
+                          
+                          
+    i = 0
+    while True:
+        i += 1
+        message = {'datetime': datetime.now().__str__(), 'iteration': i} 
+        attributes = {'messageId': str(uuid.uuid4()), 'iteration': i}
+        
+        print(json.dumps(message))
+        print(json.dumps(attributes))
+    
+        if user.are_temporary_credentials_valid() is False:
+            temp_credentials = user.get_temporary_credentials(True)
+            
+            client = boto3.client('sns',
+                                  aws_access_key_id=temp_credentials['Credentials']['AccessKeyId'],
+                                  aws_secret_access_key=temp_credentials['Credentials']['SecretKey'],
+                                  aws_session_token=temp_credentials['Credentials']['SessionToken'])
+        
+        publish_message(client, 
+                        args.topicArn,
+                        json.dumps(message),
+                        attributes)
+        
+        # time.sleep(60)
+        break
+    
+    return
+
+
+        
+def publish_message(client, topicArn, message, attributes):
+
+    try:
+        att_dict = {}
+        for key, value in attributes.items():
+            if isinstance(value, str):
+                att_dict[key] = {'DataType': 'String', 'StringValue': value}
+            elif isinstance(value, bytes):
+                att_dict[key] = {'DataType': 'Binary', 'BinaryValue': value}
+                
+        response = client.publish(TopicArn=topicArn,
+                                  Message=message, 
+                                  MessageAttributes=att_dict)
+                                  
+        message_id = response['MessageId']
+        
+        print(f'Message published: {message_id}')
+    except Exception as e:
+        exception_type = type(e).__name__
+        print(exception_type)
+        print(e)
+    else:
+        return message_id
+
+if __name__ == "__main__":
+    main()
+    
---
+++ CognitoUser.py
@@ -0,0 +1,117 @@
+#!/bin/env python3
+import boto3
+import json
+from datetime import datetime
+from datetime import tzinfo
+from pycognito import Cognito #pip install pycognito
+from dateutil.tz import tzlocal
+
+class CognitoUser:
+    _cognitoUser = None
+    _region = "eu-west-1"
+    _credentials = None
+    
+    def __init__(
+        self,
+        username,
+        password,
+        user_pool_id,
+        client_id,
+        identity_provider_id
+    ):
+        self.username=username
+        self.password=password
+        self.user_pool_id=user_pool_id
+        self.client_id=client_id
+        self.identity_provider_id=identity_provider_id
+        
+        # Create a Cognito User Object. This object will log into Cognito and obtain OAuth2.0 tokens
+        self._cognitoUser = Cognito(user_pool_id = self.user_pool_id,
+                                    client_id = self.client_id,
+                                    user_pool_region = self._region,
+                                    username=self.username)
+        
+    def sign_in(self):
+        print("Signing in")
+        try:
+            if self._cognitoUser is not None:
+                self._cognitoUser.authenticate(self.password)
+                
+            print("Signed in")
+        except Exception as e:
+            exception_type = type(e).__name__
+            print(exception_type)
+            print(e)
+            
+        
+    def get_open_id_token(self):
+        try:
+            self._cognitoUser.check_token()
+            
+            if self._cognitoUser is not None:
+                return self._cognitoUser.id_token
+            else:
+                return None
+        except Exception as e:
+            print('\n' * 2)
+            exception_type = type(e).__name__
+            print(exception_type)
+            print(e)
+            print('\n' * 2)
+            print('Resigning in: refresh token probably expired')
+            print('\n' * 2)
+            self.sign_in()
+
+    def get_temporary_credentials(self, forceCreation = False):
+        """
+        In order to access resources in AWS accounts you need credentials.I
+        These credentials are obtained with three things:
+        1) A valid Id Token
+        2) The provider that issued the Id Token
+        3) A valid Identity Id for the account you need access to.
+        """
+        provider = f'cognito-idp.{self._region}.amazonaws.com/{self.user_pool_id}'
+        open_id_token = self.get_open_id_token()
+        
+        if forceCreation is True or self.are_temporary_credentials_valid() is False:
+            try:
+                client = boto3.client('cognito-identity')
+                # Create or get the Identity in the AWS account you want to access resources in (like SNS)                          
+                identity_details = client.get_id( 
+                    IdentityPoolId=self.identity_provider_id,
+                    Logins={
+                            provider: open_id_token
+                        } )
+                        
+                identity_id = identity_details['IdentityId']
+                
+                # Get temporary credentials
+                if open_id_token is not None and identity_id is not None:
+                    
+                    self._credentials = client.get_credentials_for_identity(
+                        IdentityId=identity_id,
+                        Logins={
+                            provider: open_id_token
+                        } )
+            except Exception as e:
+                exception_type = type(e).__name__
+                print(exception_type)
+                print(e)
+                
+        return self._credentials
+    
+    def are_temporary_credentials_valid(self):
+        if self._credentials is None:
+            return False
+        
+        issued = self._credentials['Credentials']['Expiration']
+        now = datetime.now(tzlocal())
+        
+        #datetime.datetime(2021, 4, 13, 12, 2, 52, tzinfo=tzlocal())
+        if now > issued:
+            valid = True
+        else:
+            valid = False
+            
+        return valid
+        
